859987e7e6941feeaf479c851fda69ae1d62a6b8
subhramaniyan-t <subhramaniyan.t@tigeranalytics.com>
2023-03-09 00:38:58 +0530

    Updated env.yml file and non standard code

--- nonstandardcode/nonstandardcode.py
+++ nonstandardcode/nonstandardcode.py
@@ -10,7 +10,6 @@ from six.moves import urllib
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
-
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     os.makedirs(housing_path, exist_ok=True)
     tgz_path = os.path.join(housing_path, "housing.tgz")
@@ -20,12 +19,13 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz.close()
 
 import pandas as pd
-
+fetch_housing_data() 
 def load_housing_data(housing_path=HOUSING_PATH):
+    print(HOUSING_PATH)
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
-housing = load_housing_data
+housing = load_housing_data()
 
 from sklearn.model_selection import train_test_split
 
